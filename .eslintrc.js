3f76136f25393c4f4bdc043e67b29cd694357d95
David Tran <davidlamt@gmail.com>
2020-06-28 15:34:03 -0700

    Add support for Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,9 @@ module.exports = {
       "eslint:recommended", // Use the recommened rules from eslint
       "plugin:@typescript-eslint/recommended", // Use the recommended rules from @typescript-eslint/eslint-plugin
       "plugin:react/recommended", // Use the recommended rules from eslint-plugin-react
+      "prettier/@typescript-eslint", // Use eslint-config-prettier to disable ESLint formatting rules from @typescript-eslint/eslint-plugin that would conflict with Prettier
+      "prettier/react", // Use eslint-config-prettier to disable ESLint formatting rules from eslint-plugin-react that would conflict with with Prettier
+      "plugin:prettier/recommended", // Enables eslint-plugin-prettier to display Prettier errors as ESLint errors
   ],
   parser: "@typescript-eslint/parser", // Specifies the ESLint parser
   parserOptions: {
@@ -20,6 +23,7 @@ module.exports = {
   },
   plugins: [
       "@typescript-eslint", // Allows for manually setting @typescript-eslint/* rules 
+      "prettier", // Allows for manually setting prettier/* rules
       "react", // Allows for manually setting react/* rules
   ],
   settings: {
