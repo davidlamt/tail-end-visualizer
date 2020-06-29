fe7039aeb4935b585b3967deb916bec695ea2bf4
David Tran <davidlamt@gmail.com>
2020-06-28 20:29:40 -0700

    Style the initial navigation bar

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const NavigationContainer = styled.nav`
+  align-items: center;
+  background-color: #282c34;
+  color: #fff;
+  display: flex;
+  min-height: 50px;
+  padding: 0 20px;
+`;
+
+const NavigationHeader = styled.h1`
+  font-weight: 700;
+  margin: 0;
+`;
 
 const NavigationBar: React.FunctionComponent = () => {
-  return <div>Navigation Bar</div>;
+  return (
+    <NavigationContainer>
+      <NavigationHeader>The Tail End Visualizer</NavigationHeader>
+    </NavigationContainer>
+  );
 };
 
 export default NavigationBar;
