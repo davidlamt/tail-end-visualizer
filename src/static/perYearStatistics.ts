6366a6c30b0d1d526e7637d4906c6feb18f4fb1b
David Tran <davidlamt@gmail.com>
2020-07-21 22:37:00 -0700

    Add 2 more per year facts

--- src/static/perYearStatistics.ts
+++ src/static/perYearStatistics.ts
@@ -5,8 +5,10 @@ interface PerYearStatistic {
 
 const perYearStatistics: PerYearStatistic[] = [
   { noun: 'breath', perYearCount: 8000000 },
+  { noun: 'day', perYearCount: 365 },
   { noun: 'heartbeat', perYearCount: 42000000 },
   { noun: 'laugh', perYearCount: 6000 },
+  { noun: 'word', perYearCount: 2555000 },
 ];
 
 export default perYearStatistics;
